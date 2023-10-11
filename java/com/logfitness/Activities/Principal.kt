419eab22e32f43a05454e968a8fb5e5b7f9227f7
Mario <msumariva98@gmail.com>
2023-10-11 11:47:02 +0200

    First commit

---
+++ java/com/logfitness/Activities/Principal.kt
@@ -0,0 +1,12 @@
+package com.logfitness.Activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.logfitness.R
+
+class Principal : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_principal)
+    }
+}
